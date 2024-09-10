074e2e0020a4e787797b141bcf9b231472e69656
EGBE Victor Jnr <victoregbejunior@gmail.com>
2024-09-10 16:14:20 +0100

    Static files collections

--- server/settings.py
+++ server/settings.py
@@ -148,6 +148,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = BASE_DIR / 'static_root/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
